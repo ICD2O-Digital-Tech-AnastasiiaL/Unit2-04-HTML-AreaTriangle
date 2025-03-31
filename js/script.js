e962bf87b26a50b5d737bd856ba5e34943d5085b
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-03-31 17:09:34 +0000

    Created area calculation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Anastasiia Lebedenko
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the area of the triangle
+ */
+function calculate () {
+  // input
+  let height = parseInt(document.getElementById('height-of-the-triangle').value);
+  let base = parseInt(document.getElementById('base-of-the-triangle').value);
+
+  // process
+  let area = (base * height) / 2;
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
